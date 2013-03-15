66a5f55b50ab983e89ab4de755a170db25249786
Miguel Guinada <mguinada@gmail.com>
2013-03-15 01:18:13 +0000

    Rails validators must not validate unless the a value is present

--- lib/citizenship/nif.rb
+++ lib/citizenship/nif.rb
@@ -4,9 +4,9 @@ module Citizenship
     id_number = String(number).delete(' ')
     first_digit_universe = [1, 2, 5, 6, 8, 9]
 
-    raise NIFError, :size if id_number.size != 9
-    raise NIFError, :prefix, prefixes: first_digit_universe.join(', ') if first_digit_universe.include?(number[0])
-    raise NIFError, :invalid_check_digit unless decimal_check_digit_match?(id_number[0..-2], id_number[-1])
+    raise NIFError.new(:size) if id_number.size != 9
+    raise NIFError.new(:prefix, prefixes: first_digit_universe.join(', ')) unless first_digit_universe.include?(id_number[0].to_i)
+    raise NIFError.new(:invalid_check_digit) unless decimal_check_digit_match?(id_number[0..-2], id_number[-1])
     number
   end
 
