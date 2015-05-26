bc739b8a14a2f0fa694f0f3d58f88cd50a906551
Sergio Freire <s.freire@runtime-revolution.com>
2015-05-26 17:35:35 +0100

    in memory zip codes validation

--- lib/citizenship/zip_code.rb
+++ lib/citizenship/zip_code.rb
@@ -1,10 +1,10 @@
 module Citizenship
-  @zip_codes={}
+  @@zip_codes={}
 
-  def self.valid_zip_code!(zip_code,options={})
+  def self.valid_zip_code!(zip_code,options={ use_db: :in_memory})
     if options[:full_validation] == true
 	load_zip_codes_db_if_necessary
-	raise ZipCodeError, :invalid_zip_code unless zip_codes.has_key?(zip)
+	raise ZipCodeError, :invalid_zip_code unless @@zip_codes.has_key?(zip_code)
     else
     	regexp = /^(\d{4})-(\d{3})$/
     	raise ZipCodeError, :invalid_zip_code unless zip_code.match(regexp)
@@ -13,19 +13,21 @@ module Citizenship
   end
 
   def self.valid_zip_code?(zip_code,options={})
-    valid_zip_code!(zip_code)
+    valid_zip_code!(zip_code,options)
     true
   rescue ZipCodeError
     false
   end
 
-  private
-  def load_zip_codes_db_if_necessary
-	if @zip_codes.empty?
-	  File.foreach('lib/assets/zipcodes.db') do |line|
-	    arr=line.chomp.split(";")
-	    zip="#{arr[0]}-#{arr[1]}"
-	    @zip_codes[zip]=1
+#  private
+
+  def self.load_zip_codes_db_if_necessary
+	if @@zip_codes.empty?
+	  path= File.join(File.dirname(__FILE__), '..'.freeze, 'assets',)
+	  File.foreach('lib/assets/zipcodes.db') do |zip|
+	    #arr=line.chomp.split(";")
+	    #zip="#{arr[0]}-#{arr[1]}"
+	    @@zip_codes[zip.chomp]=1
 	  end
 	end
   end
--- spec/citizenship/zip_code_spec.rb
+++ spec/citizenship/zip_code_spec.rb
@@ -1,6 +1,8 @@
 describe 'Zip code validation' do
   it 'passes for valid Zip codes' do
     expect(Citizenship.valid_zip_code?('1000-100')).to be_truthy
+    expect(Citizenship.valid_zip_code?('1200-214', full_validation: true)).to be_truthy
+    expect(Citizenship.valid_zip_code?('1200-999', full_validation: true)).to be_falsy
   end
 
   it 'fails for invalid Zip codes' do
